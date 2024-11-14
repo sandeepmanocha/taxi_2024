153f25602e9ed3c2091f2b81be6ab383a53e8acf
Sandeep Manocha <sandeepmanocha@google.com>
2024-11-14 15:37:06 +0000

    ccc

---
+++ includes/source_declarations.js
@@ -0,0 +1,25 @@
+const tables = {
+    "taxi_trips": {
+        "table": "taxi_trips",
+        "dataset": dataform.projectConfig.vars.TAXI_DATASET,
+        "project": dataform.projectConfig.vars.SOURCE_PROJECT,
+    },
+    "zip_codes": {
+        "table": "zip_codes",
+        "dataset": dataform.projectConfig.vars.GEO_BOUNDARY_DATASET,
+        "project": dataform.projectConfig.vars.SOURCE_PROJECT,
+    }   
+}
+
+
+function declare_tables(mapping) {
+    Object.keys(mapping).forEach(table => {
+        dataform.declare({
+            database: mapping[table]["project"],
+            schema: mapping[table]["dataset"],
+            name: mapping[table]["table"]
+        });
+    });
+}
+ 
+declare_tables(tables)
